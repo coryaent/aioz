b556de82a913497ca2200cc5b75245f46b864383
Stephen Corya <stephen@corya.net>
2024-01-16 13:36:21 -0500

    update entrypoint with folder creation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 sed -i s/#PermitRootLogin\ prohibit-password.*/PermitRootLogin\ prohibit-password/ /etc/ssh/sshd_config
+mkdir -p /root/.ssh
 /usr/sbin/sshd
 if ! test -f /root/.aiozworker/privkey.json; then
+  mkdir -p /root/.aiozworker/
   aioznode keytool new | jq -r .priv_key > /root/.aiozworker/privkey.json
   echo "Created new private key"
 fi
-aioznode start --priv-key-file /root/.aiozworker/privkey.json
+aioznode "$@"
